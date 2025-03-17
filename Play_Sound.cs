4d5673cd38a099524e524e89d2da5ad345e442f3
Mafoko-Reotshepile <163289791+Mafoko-Reotshepile@users.noreply.github.com>
2025-03-17 07:27:53 +0000

    Entered method to play sound

--- Play_Sound.cs
+++ Play_Sound.cs
@@ -1,10 +1,18 @@
 ﻿namespace ASC_Desing_2
 {
+    using System.Media;
+    using System;
     public class Play_Sound
     {
         public Play_Sound()
         {
 
+        }
+
+        private void Play_Now(string full_path) 
+        { 
+        
+        
         }
     }
 }
