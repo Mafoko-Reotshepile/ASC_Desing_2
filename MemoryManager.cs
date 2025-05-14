71f5402ba8f2ba522c6869be392be30ce7060297
Mafoko-Reotshepile <163289791+Mafoko-Reotshepile@users.noreply.github.com>
2025-05-14 09:14:52 +0000

    Created class for memory management

---
+++ MemoryManager.cs
@@ -0,0 +1,11 @@
+﻿namespace ASC_Desing_2
+{
+    internal class MemoryManager
+    {
+        public MemoryManager()
+        {
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace ASC_Desing_2
             new Show_ASC() { };
             new User_Input() { };
             new User_Interaction() { };
+            new MemoryManager() { };
            
             
         }
